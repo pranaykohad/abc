5c8e9fd51638521a00b217c3eb5b8e123ad493e6
pranaykohad <prakohad@gmail.com>
2020-01-11 10:57:12 +0530

    first

---
+++ apiDemo/src/main/java/com/api/apiDemo/ApiDemoApplication.java
@@ -0,0 +1,28 @@
+package com.api.apiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiDemoApplication {
+	
+
+
+	@GetMapping
+	public String getdataping() {
+		return "spring boot";
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ApiDemoApplication.class, args);
+		System.out.println("project started..............")
+	}
+	
+	
+	
+}
